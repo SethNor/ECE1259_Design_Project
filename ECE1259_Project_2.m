c94794e9c7139d2a5b96f836fa5839ea978ebd7a
Seth <94641030+SethNor@users.noreply.github.com>
2023-04-03 11:16:30 -0400

    Add files via upload

---
+++ ECE1259_Project_2.m
@@ -0,0 +1,309 @@
+% Rick Brophy
+% This program is to simulate a coax cable design
+
+format long e
+clear
+clc
+
+%% opening statements
+disp('This program is for coxial cable design')
+disp('---------------------------------------')
+disp('ECE1249 Design Project 2')
+disp('Authors: Rick, Seth, Andrew, Kamden')
+fprintf('\n\n')
+
+%% constants
+h = 6.626e-34; % planks constant in Js
+u_0 = pi*4e-7; % permeability constant in H/m
+e_0 = 8.854e-12; % permitivitty constant F/m
+c = 3e8; % speed of light in m/s
+
+% grab these from the material data
+sigma_d = 0;
+sigma_c = 0;
+e_r = 0;
+u_r = 0;
+Ebr = 0;
+tan_d = 0;
+z_l = 0;
+z_s = 0;
+
+%% inputs
+%Listed conductors with sigma_c
+Al = 3.8e7;
+Car = 3e4;
+Cu = 5.8e7;
+Au = 4.1e7;
+Gr = 7e4;
+Fe = 1e7;
+Pb = 5e6;
+Nich = 1e6;
+Ni = 1.5e7;
+Ag = 6.2e7;
+Sol = 7e6;
+Stain = 1.1e6;
+Sn = 8.8e6;
+W = 1.8e7;
+
+%Listed dielectrics with e_r, Ebr, tand, and sigma_d
+Air = [1.0005, 3e6, 0, 0];
+Alum = [9.9, 0, 0.0001, 0];
+Bar = [1200, 7.5e6, 0, 0];
+Glass = [10, 30e6, 0.004, 10^-12];
+Ice = [4.2, 0, 0.12, 0];
+Mica = [5.4, 200e6, 0.0003, 10^-15];
+Poleth = [2.26, 47e6, 0, 10^-16];
+Polsty = [2.56, 20e6, 0, 10^-17];
+Quartz = [3.8, 30e6, 0.0002, 10^-17];
+Si = [11.8, 0, 0, 4.4e-4];
+Soil = [3, 0, 0.017, 2e-3]; %e_r between 3-4 so I put 3
+Teflon = [2.1, 60e6, 0.0001, 10^-15]; %tand < 0.0002 so I put 0.0001
+Water = [81, 0, 0.04, 10^-4];
+Seawater = [72, 0, 0.9, 5];
+
+%creating a menu for the user to give conductor 
+conductor = menu('What conductor are you using?', 'Aluminum', 'Carbon', 'Copper', 'Gold', 'Graphite', 'Iron', 'Lead', 'Nichrome', 'Nickel', 'Silver', 'Solder', 'Stainless Steel', 'Tin', 'Tungsten', 'Other');
+switch conductor
+    case 1
+        sigma_c = Al;
+    case 2
+        sigma_c = Car;
+    case 3
+        sigma_c = Cu;
+    case 4
+        sigma_c = Au;
+    case 5
+        sigma_c = Gr;
+    case 6
+        sigma_c = Fe;
+    case 7
+        sigma_c = Pb;
+    case 8
+        sigma_c = Nich;
+    case 9 
+        sigma_c = Ni;
+    case 10
+        sigma_c = Ag;
+    case 11
+        sigma_c = Sol;
+    case 12
+        sigma_c = Stain;
+    case 13
+        sigma_c = Sn;
+    case 14 
+        sigma_c = W;
+    case 15
+        sigma_c = input('Please enter a value for the conductor permiability in S/m: ');
+end
+
+%Menu for the dielectric from list or given values.
+dielectric = menu('What dielectric are you using?', 'Air', 'Alumina', 'Barium Titanate', 'Glass', 'Ice', 'Mica', 'Polyethylene', 'Polystyrene', 'Quartz (fused)', 'Silicon (pure)', 'Soil (dry)', 'Teflon', 'Water (distilled)', 'Seawater', 'Other');
+switch dielectric
+    case 1
+        e_r = Air(1);
+        Ebr = Air(2);
+        tan_d = Air(3);
+        sigma_d = Air(4);
+    case 2
+        e_r = Alum(1);
+        Ebr = Alum(2);
+        tan_d = Alum(3);
+        sigma_d = Alum(4);
+    case 3
+        e_r = Bar(1);
+        Ebr = Bar(2);
+        tan_d = Bar(3);
+        sigma_d = Bar(4);
+    case 4
+        e_r = Glass(1);
+        Ebr = Glass(2);
+        tan_d = Glass(3);
+        sigma_d = Glass(4); 
+    case 5
+        e_r = Ice(1);
+        Ebr = Ice(2);
+        tan_d = Ice(3);
+        sigma_d = Ice(4); 
+    case 6
+        e_r = Mica(1);
+        Ebr = Mica(2);
+        tan_d = Mica(3);
+        sigma_d = Mica(4); 
+    case 7
+        e_r = Poleth(1);
+        Ebr = Poleth(2);
+        tan_d = Poleth(3);
+        sigma_d = Poleth(4); 
+    case 8 
+        e_r = Polsty(1);
+        Ebr = Polsty(2);
+        tan_d = Polsty(3);
+        sigma_d = Polsty(4); 
+    case 9
+        e_r = Quartz(1);
+        Ebr = Quartz(2);
+        tan_d = Quartz(3);
+        sigma_d = Quartz(4); 
+    case 10
+        e_r = Si(1);
+        Ebr = Si(2);
+        tan_d = Si(3);
+        sigma_d = Si(4); 
+    case 11
+        e_r = Soil(1);
+        Ebr = Soil(2);
+        tan_d = Soil(3);
+        sigma_d = Soil(4); 
+    case 12
+        e_r = Teflon(1);
+        Ebr = Teflon(2);
+        tan_d = Teflon(3);
+        sigma_d = Teflon(4); 
+    case 13
+        e_r = Water(1);
+        Ebr = Water(2);
+        tan_d = Water(3);
+        sigma_d = Water(4); 
+    case 14
+        e_r = Seawater(1);
+        Ebr = Seawater(2);
+        tan_d = Seawater(3);
+        sigma_d = Seawater(4); 
+    case 15
+        e_r = input('Please enter a value for the relative permitivity of the dielectric: ');
+        Ebr = input('Please enter a value for the Electric field breakdown in V/m: ');
+        tan_d = input('Please enter a value for the tangent of the dielectric phase at 1MHz: ');
+        sigma_d = input('Please enter a value for the permiability of the dielectric in S/m: ');
+end
+        
+    % somehow make a struct of all the materials to grab data from to find
+    % information about conductivity, permitivitty, 
+% asks the user to enter several design requirements
+a = input('Please enter a value for the inner radius in mm: ');
+b = input('Please enter a value for the outer radius in mm: ');
+f = input('Please enter a value for the operating frequency in Hz: ');
+l = input('Please enter the length in m: ');
+v = input('Please enter the operating voltage in V: '); % IS IT USUAL TO KNOW THIS
+%mat_d = input('Please enter the type of material for the dielectric: ', "s");
+%mat_c = input('Please enter the type of material for the conductor: ', "s");
+
+%% outputs - lossy
+w = 2 * pi * f; % angular freq
+j = sqrt(-1); % imaginary number
+G = 2 * pi * sigma_d / log(b / a); % conductance
+C = 2 * pi * e_r * e_0 / log(b / a); % capacitance
+L = u_r * u_0 * log(b / a) / (2 * pi); % inductance
+R = (1 / (2 * pi)) * ((1 / a) +(1 / b)) * sqrt(pi * f * u_0 / sigma_c); % resistance
+gamma = sqrt((R + j * w * L) * (G + j * w * C)); % propagation constant
+z_0 = (R + j * w * L) / gamma; % characteristic impednace 
+alpha = real(gamma); % attenuation constant
+beta = imag(gamma); % phase constant
+wl = 2 * pi / beta; % wavelength
+u_p = w / beta; % propagation velocity
+p_avg = 0; % look at lecture slides
+g_dB = 10 * log10(exp(-2 * alpha * l)); % gain attenuation in dB
+reflect = (z_l - z_0) / (z_l + z_0); % reflection coefficient
+z_in = z_0 * (z_l + z_0 * tanh(gamma * l)) / (z_0 + z_l * tanh(gamma * l)); % input impedance
+v_in = 3 * (z_in / (z_in + z_s)); % input voltage - need z_s? 
+v_0p = v_in / (exp(gamma * l) + reflect*exp(-gamma * l)); % received voltage 
+v_0n = reflect * v_0p; % reflected voltage
+v_l = v_0p + v_0n; % load voltage
+VSWR = (1 + abs(reflect)) / (1 - abs(reflect)); % voltage standing wave ratio
+
+%% outputs - lossless
+w = 2 * pi * f; % angular freq
+j = sqrt(-1); % imaginary number
+G = 2 * pi * sigma_d / log(b / a); % conductance
+C = 2 * pi * e_r * e_0 / log(b / a); % capacitance
+L = u_r * u_0 * log(b / a) / (2 * pi); % inductance
+R = (1 / (2 * pi)) * ((1 / a) +(1 / b)) * sqrt(pi * f * u_0 / sigma_c); % resistance
+beta = w * sqrt(L * C); % phase constant
+gamma = j * beta; % propagation constant
+z_0 = sqrt(L / C); % characteristic impedance
+wl = 2 * pi / beta; % wavelength
+u_p = w / beta; % propagation velocity
+p_avg = 0; % look at lecture slides
+reflect = (z_l - z_0) / (z_l + z_0); % reflection coefficient
+z_in = z_0 * (z_l + z_0 * tan(beta * l)) / (z_0 + z_l * tan(beta * l)); % input impedance
+v_in = 3 * (z_in / (z_in + z_s)); % input voltage - need z_s? 
+v_0p = v_in / (exp(gamma * l) + reflect * exp(-gamma * l)); % received voltage 
+v_0n = reflect * v_0p; % reflected voltage
+v_l = v_0p + v_0n; % load voltage
+VSWR = (1 + abs(reflect)) / (1 - abs(reflect)); % voltage standing wave ratio
+
+%% Display
+disp(['alpha = ', num2str(alpha), ' Np/m'])
+disp(['beta = ', num2str(beta), ' rad/m'])
+disp(['Z_0 = ', num2str(z_0), ' Ohms'])
+disp(['Reflection coefficient = ', num2str(reflect)])
+disp(['Z_in = ', num2str(z_in), ' Ohms'])
+disp(['V_in = ', num2str(v_in), ' V'])
+disp(['V_L = ', num2str(v_l), ' V'])
+disp(['G_db = ', num2str(G_db), ' dB'])
+
+%% load check - I dont think you can do much without a load
+msg_load_check = "Do you plan to connect a load?"; % ask the user if there is a load 
+opts_load_check = ["Yes" "No"]; % options
+choice_load_check = menu(msg_load_check, opts_load_check); % create interactive menu
+
+if choice_load_check == 1 % there IS a load 
+    disp('The system now has a load! Is this correct?')
+    check = input('Enter yes or no: ', "s"); % confirm with user of choice
+
+    if check == "yes" || check == "Yes"
+        % complete the rest of the calculations
+
+    else
+        disp('You will have to restart the program.');
+        return;
+       
+    end
+
+end
+
+if choice_load_check == 2 % there ISNT a load 
+    disp('The system doesnt have a load! Is this correct?')
+    check = input('Enter yes or no: ', "s"); % confirm with user of choice
+
+    if check == "yes" || check == "Yes"
+        % complete the rest of the calculations
+
+    else
+        disp('You will have to restart the program.');
+        return;
+       
+    end
+end
+
+%% lossy vs lossless
+msg_loss = "Is your system lossless or lossy?"; % ask the user if there is loss
+opts_loss = ["Lossy" "Lossless"]; % options
+choice_loss = menu(msg_loss, opts_loss); % create interactive menu
+
+if choice_load_check == 1 % system is LOSSY
+    disp('The system is now lossy! Is this correct?')
+    check = input('Enter yes or no: ', "s"); % confirm with user of choice
+
+    if check == "yes" || check == "Yes"
+        % complete the rest of the calculations
+
+    else
+        disp('You will have to restart the program.');
+        return;
+       
+    end
+end
+
+if choice_load_check == 2 % system is LOSSLESS
+    disp('The system is now lossless! Is this correct?')
+    check = input('Enter yes or no: ', "s"); % confirm with user of choice
+
+    if check == "yes" || check == "Yes"
+        % complete the rest of the calculations
+
+    else
+        disp('You will have to restart the program.');
+        return;
+       
+    end
+end
